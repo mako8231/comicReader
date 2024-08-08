18945ab57dd4e401c6bb6eb8bedb6ff3a6966d7c
mako8231 <mm.neto2007@gmail.com>
2024-08-08 10:19:33 -0400

    make front page a little lil pretty

--- comic-reader-front/src/views/HomeView.vue
+++ comic-reader-front/src/views/HomeView.vue
@@ -5,30 +5,38 @@
 
 <template>
   <NavBar></NavBar>
-  <div id="popular">
-    <h2 class="section-subtitle">Popular Comics</h2>
-    <div class="comics-section">
-      <div class="comic-card">
-        <img class="comic-thumbnail" src="https://cdn.discordapp.com/attachments/231829187776741377/1270820527304343653/tq9jch4n6g5b1.png?ex=66b51758&is=66b3c5d8&hm=cdc96b322063ca97ca586b1f2efecf2db195738abeeb93a5cd2a941f76229d4c&" alt="">
-        <p>A lenda do dlagao neglo e a volta do lei lucio da silva capitulo 400</p>
-      </div>
-      <div class="comic-card">
-        <img class="comic-thumbnail" src="https://cdn.discordapp.com/attachments/231829187776741377/1270820527304343653/tq9jch4n6g5b1.png?ex=66b51758&is=66b3c5d8&hm=cdc96b322063ca97ca586b1f2efecf2db195738abeeb93a5cd2a941f76229d4c&" alt="">
-        <p>A lenda do dlagao neglo e a volta do lei lucio da silva capitulo 400</p>
-      </div>
-      <div class="comic-card">
-        <img class="comic-thumbnail" src="https://cdn.discordapp.com/attachments/231829187776741377/1270820527304343653/tq9jch4n6g5b1.png?ex=66b51758&is=66b3c5d8&hm=cdc96b322063ca97ca586b1f2efecf2db195738abeeb93a5cd2a941f76229d4c&" alt="">
-        <p>A lenda do dlagao neglo e a volta do lei lucio da silva capitulo 400</p>
-      </div>
+  <h2 class="section-subtitle">Popular Comics</h2>
+    
+  <div class="" id="popular">
+    <div class="container comics-section">
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-lg-4">
+          <div class="comic-card">
+          <img class="rounded mx-auto d-block img-thumbnail comic-thumbnail" src="https://media.discordapp.net/attachments/231829187776741377/1271109748997099560/IMG_7191.png?ex=66b624b4&is=66b4d334&hm=e1b3e386a245c7d02e37f1b1bc93e61aae230204df060ece9607708c5ae7c0c1&=&format=webp&quality=lossless&width=354&height=350" alt="">
+          <p class="comic-title">A lenda do dlagao neglo e a volta do lei lucio da silva capitulo 400</p>          
+          </div>
+        </div>
+    
+        <div class="col-xs-12 col-sm-12 col-lg-4">
+          <div class="comic-card">
+          <img class="rounded mx-auto d-block img-thumbnail comic-thumbnail" src="https://cdn.discordapp.com/attachments/231829187776741377/1270820527304343653/tq9jch4n6g5b1.png?ex=66b51758&is=66b3c5d8&hm=cdc96b322063ca97ca586b1f2efecf2db195738abeeb93a5cd2a941f76229d4c&" alt="">
+          <p class="comic-title">A lenda do dlagao neglo e a volta do lei lucio da silva capitulo 400</p>          
+          </div>
+        </div>
+    
+        <div class="col-xs-12 col-sm-12 col-lg-4">
+          <div class="comic-card">
+          <img class="rounded mx-auto d-block img-thumbnail comic-thumbnail" src="https://cdn.discordapp.com/attachments/231829187776741377/1270820527304343653/tq9jch4n6g5b1.png?ex=66b51758&is=66b3c5d8&hm=cdc96b322063ca97ca586b1f2efecf2db195738abeeb93a5cd2a941f76229d4c&" alt="">
+          <p class="comic-title">A lenda do dlagao neglo e a volta do lei lucio da silva capitulo 400</p>          
+          </div>
+        </div>
 
-      <div class="comic-card">
-        <img class="comic-thumbnail" src="https://cdn.discordapp.com/attachments/231829187776741377/1270820527304343653/tq9jch4n6g5b1.png?ex=66b51758&is=66b3c5d8&hm=cdc96b322063ca97ca586b1f2efecf2db195738abeeb93a5cd2a941f76229d4c&" alt="">
-        <p>A lenda do dlagao neglo e a volta do lei lucio da silva capitulo 400</p>
+        
       </div>
     </div>
   </div>
 
   <div id="recent">
-    <h2 class="section-subtitle"> Recently Added</h2>
+    <h2 class="container section-subtitle"> Recently Added</h2>
   </div>
 </template>
